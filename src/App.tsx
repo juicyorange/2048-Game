396d8e738cfe51288ff1250b843b09462f60c567
juicyorange <rlaxodud9809@khu.ac.kr>
2021-05-17 04:07:51 +0900

    add : 2048 game board

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
+import { useState } from 'react';
 import './App.css';
 
+// 어떤 방향으로 밀때 배열의 가장 왼쪽 s, 그다음 f
+// [0 , 0 ,0 ,0]
+//  s   f
+// 1) number[s] == 0 number[f] == 0  -> f++
+// 2) number[s] != 0 number[f] == 0 => f++
+// 3) number[s] == 0 number[f] != 0 ->  number[s] = number[f] , s++ , f=s+1
+// 3) number[s] != 0 number[f] !=0 && number[s] == number[f] -> number[s] = number[s]+number[f], s++, f=s+1
+
+const Board = styled.div`
+  background: #ad9d8f;
+  width: max-content;
+  margin: auto;
+  padding: 5px;
+  border-radius: 5px;
+  margin-top: 5px;
+`;
+
+const Block = styled.div`
+  height: 80px;
+  width: 80px;
+  background: lightgray;
+  margin: 3px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 45px;
+  font-weight: 800;
+  color: white;
+`;
+
 function App() {
+  // 버튼 한번을 누르면 전체가 바뀌기 때문에 그냥 하나의 state에서 grid를 관리
+  const [grid, setGrid] = useState([
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Board>
+      {grid.map((row, rowIndex) => (
+        <div style={{ display: 'flex' }} key={`rowIndex-${rowIndex}`}>
+          {row.map((item, itemIndex) => (
+            <Block key={`itemIndex-${itemIndex}`}>{item}</Block>
+          ))}
+        </div>
+      ))}
+    </Board>
   );
 }
 
